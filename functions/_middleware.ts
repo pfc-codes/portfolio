5d0f4a7658b20b18e4b32490e906b003f4b7202a
Pietro Costa <pietrofranciscosta@gmail.com>
2025-12-31 16:48:10 -0500

    removed user and pass

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,25 +1,3 @@
-export async function onRequest({ request, env, next }: any) {
-  const USER = env.BASIC_USER;
-  const PASS = env.BASIC_PASS;
-
-  const auth = request.headers.get("Authorization");
-
-  if (!auth || !auth.startsWith("Basic ")) {
-    return new Response("Authentication required", {
-      status: 401,
-      headers: { "WWW-Authenticate": 'Basic realm="Private Site"' },
-    });
-  }
-
-  const decoded = atob(auth.split(" ")[1]);
-  const [user, pass] = decoded.split(":");
-
-  if (user !== USER || pass !== PASS) {
-    return new Response("Unauthorized", {
-      status: 401,
-      headers: { "WWW-Authenticate": 'Basic realm="Private Site"' },
-    });
-  }
-
+export async function onRequest({ next }: any) {
   return next();
 }
